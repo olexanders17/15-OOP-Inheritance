be6e69cc3e28f0ee59e6a31ef670c501ab8e09f3
olexanders <aaa@ss.ty>
2016-08-26 13:37:29 +0000

    casino update

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ function Casino(qty, money) {
     this.countSM = qty;
     this.money = money;
     this.slotMachines = [];
-    this.id=0;
+    this.id = 0;
+
 
     //initial fill by slots
     this.init = function () {
         for (var i = 1; i <= qty; i++) {
-            var el=new SlotMachine(money / qty);
-            el.id=this.id+1;
+            var el = new SlotMachine(money / qty + 10 * i);
+            el.id = this.id + i;
             this.slotMachines.push(el);
         }
 
+        this.slotMachines[0].isLucky=true;
+
     }
 
-    this.getMoneyAmount = function () {
+    this.getMoneyAmountCasino = function () {
         return money;
     }
 
@@ -30,28 +33,87 @@ function Casino(qty, money) {
         return slotMachines[1];
     }
 
+    //done
     this.deleteSM = function (index) {
         var elToDelete;
         this.slotMachines.splice(index, 1);
     }
 
-
+        //DONE
     this.withdraw = function (money) {
+        var reducedMoney = money;
+
+
+        while (reducedMoney > 0) {
+            var obj = _.max(this.slotMachines, function (el) {
+                return el.money;
+            });
+
+            if (obj.money > reducedMoney) {
+                this.slotMachines.forEach(function (el) {
+                    if (el.id == obj.id) {
+                        el.money -= reducedMoney;
+                        reducedMoney = 0;
+                    }
+                })
+
+            } else {
+                this.slotMachines.forEach(function (el) {
+                    if (el.id == obj.id) {
+
+                        reducedMoney -= el.money;
+                        el.money = 0;
+                    }
+                })
+
+
+            }
+        }
+
+        this.money -= money;
+
 
     }
 
 
+
+
+    this.init.call(this);
 }
 
 function SlotMachine(money) {
     this.money = money;
+    this.isLucky=false;
     this.id;
+
+    this.getMoney=function () {
+        return this.money;
+    }
+    
+    this.withdraw=function (money) {
+        this.money-=money;
+    }
+
+    this.putMoney=function (money) {
+        this.money+=money;
+    }
+    
+    this.doGame=function () {
+
+    }
+    
+
 }
 
 
 var casino = new Casino(3, 100);
-casino.init();
-//casino.deleteSM(1);
+
+console.log(casino.slotMachines[0]);
+console.log(casino.slotMachines[0].withdraw(5));
+console.log(casino.slotMachines[0]);
+
+
+
 
 console.log("casino", casino);
 
